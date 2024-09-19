5ca841f7a98b999d01662d94899d6d929f4bf2ec
dalfa <engineersoftware283@gmail.com>
2024-09-19 13:22:27 +0100

    on the banner page

--- wallet-app/src/components/HeroSection/index.js
+++ wallet-app/src/components/HeroSection/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import video from '../../Videos'
+import video from '../../Videos/video.mp4'
 import { HeroContainer,
          HeroBg,
-         VideoBg } from './HeroElements.js'
+         VideoBg,
+         HeroContent,
+         HeroH1,
+         HeroP,
+        HeroBtnWrapper,
+        Button,
+      ArrowForward,
+    ArrowRight } from './HeroElements.js'
 
 function HeroSection() {
   return (
     <>
     <HeroContainer>
         <HeroBg> 
-            <VideoBg autoplay muted loop src={video} type='video/mp4' />
+            <VideoBg autoPlay loop muted src={video} type='video/mp4' />
         </HeroBg>
+        <HeroContent>
+          <HeroH1>Vitual Banking Made Easy</HeroH1>
+          <HeroP>
+            Sign up for a new account and receive $250 in credit towards your next payment.
+          </HeroP>
+          <HeroBtnWrapper>
+            <Button to="signup">
+              Get Started {hover? <ArrowForward /> : <ArrowRight />}
+            </Button>
+          </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
     </>
   )
